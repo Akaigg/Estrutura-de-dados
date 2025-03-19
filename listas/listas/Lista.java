bafa1c7ebd9171db582ff5dee8a170e0bb2c4d40
Vitor Blume <124194664+Akaigg@users.noreply.github.com>
2025-03-18 21:16:07 -0300

    Add files via upload

---
+++ listas/listas/Lista.java
@@ -0,0 +1,14 @@
+package estruturas;
+
+public class Lista {
+
+    private No inicio;
+
+    public void Lista() {
+        this.inicio = null;
+    }
+}
+
+// vetor fica contiguamente na memória
+// lista encadeada fica espalhada na memória
+
